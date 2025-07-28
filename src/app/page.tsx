2171307bce127a35c6c33f7135e4cfe6a1bdc63e
Moscart <danieltheo.73@gmail.com>
2025-07-28 19:54:58 +0700

    feat: enhance ESLint and Prettier configuration; add formatting rules and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+  [1, 2, 3, 4].map((item) => {
+    return item * 2;
+  });
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
