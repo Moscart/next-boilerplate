7426f9bdf3b06520eb710e35ed8e768aed10f40c
Moscart <danieltheo.73@gmail.com>
2025-07-30 18:18:29 +0700

    feat: remove unused mapping function from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 
 export default function Home() {
-  [1, 2, 3, 4].map((item) => {
-    return item * 2;
-  });
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
